2c5f7f96c5645b6ee335157eef835ddc8b42d9d5
Sergey Bogatyrets <serj.sj@gmail.com>
2017-06-02 15:12:08 +0300

    Up pr test

--- .travis/docker_builder.py
+++ .travis/docker_builder.py
@@ -19,7 +19,7 @@ dirs = [
   "ubuntu"
 ]
 
-s = subprocess.check_output(['git', 'diff-tree', tag, '--name-only','-m', '--pretty='])
+s = subprocess.check_output(['git', 'diff-tree', os.environ['TRAVIS_BRANCH'], '--name-only','-m', '--pretty='])
 changed_dirs = s.split('\n')
 
 # print(changed_dirs)
